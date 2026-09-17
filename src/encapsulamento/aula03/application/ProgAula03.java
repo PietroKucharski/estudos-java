ad19339e8dcf2488469999d9aea60b42d57948bd
pietr <pietrokucharski@hotmail.com>
2026-09-17 09:48:09 -0300

    feat: Aula 03 sobre encapsulamento

---
+++ src/encapsulamento/aula03/application/ProgAula03.java
@@ -0,0 +1,51 @@
+package encapsulamento.aula03.application;
+
+import encapsulamento.aula03.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgAula03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+//        System.out.print("Quantity in stock: ");
+//        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price);
+
+
+        System.out.println();
+
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantityToBeAdded = sc.nextInt();
+        product.addProducts(quantityToBeAdded);
+
+        System.out.println();
+
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        int quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+
+        System.out.println("Updated data: " + product);
+        sc.close();
+    }
+}
---
+++ src/encapsulamento/aula03/entities/Product.java
@@ -0,0 +1,58 @@
+package encapsulamento.aula03.entities;
+
+public class Product {
+    // Encapsulamento
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {}
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double totalValueInStock() {
+        return quantity * price;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product data:" + name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ " + String.format("%.2f", totalValueInStock());
+    }
+}
