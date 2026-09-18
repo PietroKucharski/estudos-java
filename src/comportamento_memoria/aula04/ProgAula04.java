7fc23cd302bb2b78e707bca078f9e28283018aff
pietr <pietrokucharski@hotmail.com>
2026-09-18 10:29:14 -0300

    feat: Aula 04 aprendendo sobre foreach

---
+++ src/comportamento_memoria/aula04/ProgAula04.java
@@ -0,0 +1,12 @@
+package comportamento_memoria.aula04;
+
+public class ProgAula04 {
+    public static void main(String[] args) {
+        String[] vect = new  String[] {"Pietro", "Vittor", "Rafael"};
+
+        // Aprendendo Foreach
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+    }
+}
